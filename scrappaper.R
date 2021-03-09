5d793460103f359ad715fdb013b9ebdf834d9d8b
Tyler <31669874+PolloPequeno@users.noreply.github.com>
2021-03-08 19:45:20 -0500

    more commits

---
+++ scrappaper.R
@@ -0,0 +1,15 @@
+library(devtools)
+library(fs)
+require(tidyverse)
+library(foofactors)
+library(testthat)
+
+
+a <- factor(c("character", "hits", "your", "eyeballs"))
+a
+b <- factor(c("but", "integer", "where it", "counts"))
+b
+c(a, b)
+
+
+fbind(a, b)
